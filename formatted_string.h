9bd439f7e88d7e1912f53148409e1515ce97162e
paulhenr <voxskynned@gmail.com>
2024-03-20 10:23:00 -0300

    added function to return long/uint/unsigned long string

--- formatted_string.h
+++ formatted_string.h
@@ -6,7 +6,7 @@
 /*   By: paulhenr <paulhenr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 16:55:12 by paulhenr          #+#    #+#             */
-/*   Updated: 2024/03/19 16:56:17 by paulhenr         ###   ########.fr       */
+/*   Updated: 2024/03/20 10:22:38 by paulhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,17 @@
 
 # include <stdarg.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 char	*get_fstring(const char *format, ...);
 char	*get_fstringb(const char *format, ...);
 char	*ft_sprintf(char *buffer, const char *format, ...);
+
+// get_nbr*.c
+char	*get_intstr(int nbr);
+char	*get_longstr(long nbr);
+char	*get_uintstr(unsigned int nbr);
+char	*get_luintstr(unsigned long nbr);
+
 
 #endif
--- get_nbr2.c
+++ get_nbr2.c
@@ -1,81 +1,67 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_nbr.c                                          :+:      :+:    :+:   */
+/*   get_nbr2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: paulhenr <paulhenr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/20 10:11:21 by paulhenr          #+#    #+#             */
-/*   Updated: 2024/03/20 10:17:23 by paulhenr         ###   ########.fr       */
+/*   Created: 2024/03/20 10:12:28 by paulhenr          #+#    #+#             */
+/*   Updated: 2024/03/20 10:21:38 by paulhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "formatted_string.h"
 
-static int	get_digit_len(int nbr)
+static int	get_udigit_len(unsigned int nbr)
 {
 	if (nbr == 0)
 		return (1);
-	return ((nbr > 9 || nbr < -9) + get_digit_len(nbr / 10));
+	return ((nbr > 9) + get_udigit_len(nbr / 10));
 }
 
-static int	get_ldigit_len(long nbr)
+static int	get_ludigit_len(unsigned long nbr)
 {
 	if (nbr == 0)
 		return (1);
-	return ((nbr > 9 || nbr < -9) + get_ldigit_len(nbr / 10));
+	return ((nbr > 9) + get_ludigit_len(nbr / 10));
 }
 
-char	*get_longstr(long nbr)
+char	*get_uintstr(unsigned int nbr)
 {
-	long	rem;
 	char	*nbstr;
 	size_t	index;
 
-	index = get_ldigit_len(nbr);
-	nbstr = malloc((index + 1 + (nbr < 0)) * sizeof(char));
+	index = get_udigit_len(nbr);
+	nbstr = malloc((index + 1) * sizeof(char));
 	if (!nbstr)
 		return (NULL);
-	index += (nbr < 0);
 	nbstr[index] = '\0';
-	if (nbr < 0)
-		*nbstr = '-';
 	while (1)
 	{
 		index--;
-		rem = nbr % 10;
-		if (rem < 0)
-			rem = -rem;
-		nbstr[index] = rem + '0';
-		if (&nbstr[index] == nbstr || index == 1 && *nbstr == '-')
+		nbstr[index] = (nbr % 10) + '0';
+		if (index == 0)
 			break ;
 		nbr /= 10;
 	}
 	return (nbstr);
 }
 
-char	*get_intstr(int nbr)
+char	*get_luintstr(unsigned long nbr)
 {
-	int		rem;
 	char	*nbstr;
 	size_t	index;
 
-	index = get_digit_len(nbr);
-	nbstr = malloc((index + 1 + (nbr < 0)) * sizeof(char));
+	index = get_ludigit_len(nbr);
+	nbstr = malloc((index + 1) * sizeof(char));
 	if (!nbstr)
 		return (NULL);
-	index += (nbr < 0);
 	nbstr[index] = '\0';
-	if (nbr < 0)
-		*nbstr = '-';
 	while (1)
 	{
 		index--;
-		rem = nbr % 10;
-		if (rem < 0)
-			rem = -rem;
-		nbstr[index] = rem + '0';
-		if (&nbstr[index] == nbstr || index == 1 && *nbstr == '-')
+		nbstr[index] = (nbr % 10) + '0';
+		if (index == 0)
 			break ;
 		nbr /= 10;
 	}
